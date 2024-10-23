30d66b9366fab1d5683de6fcb5128a8d459856ea
Mokmedin <mokmedini@gmail.com>
2024-10-23 20:47:10 +0600

    tailwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {useState(0)
+
+  return (
+    <>
+      <h1 className='text-6xl bg-red-300'>Tailwind Knowledge</h1>
+    </>
+  )
+}
+
+export default App

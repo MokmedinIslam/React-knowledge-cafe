3ca15ff7047148123f4685c87d0779ca26b2ddb0
Mokmedin <mokmedini@gmail.com>
2024-10-24 13:33:04 +0600

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
 
 function App() {useState(0)
 
   return (
     <>
-      <h1 className='text-6xl bg-red-300'>Tailwind Knowledge</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
